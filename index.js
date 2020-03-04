17b57f043e86ba72b3cd0156b174935c0421b793
MohanapriyaLingusamy <priyalingusamy.lmp@gmail.com>
2020-03-04 18:42:29 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-// Import stylesheets
-import './style.css';
+//Addition
+function add(val1,val2)
+{
+  return val1+val2;
+}
+let varr=add(2,2)
+console.log(varr)
+//output:4
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//Subtration
+function sub(val1,val2)
+{
+  return val1-val2;
+}
+let varr=sub(2,2)
+console.log(varr)
+//output:0
+
+//Multiplication
+function mul(val1,val2)
+{
+  return val1*val2;
+}
+let varr=mul(2,2)
+console.log(varr)
+//output:4
+
+//Division
+div=(val1,val2)=>{
+  return val1/val2;
+}
+console.log(div(2,2))
+//output:1
+
+//Modulo
+mod=(val1,val2)=>{
+  return val1%val2;
+}
+console.log(mod(2,2))
+//output:0
+
+//compare number
+comNum=(val1,val2)=>{
+  return (val1===val2)?true:false
+}
+console.log(comNum(2,2))
